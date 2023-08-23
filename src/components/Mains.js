7dca6d9ba047d05d048cb041fb5e2ebefbeca412
Abhishekdevelopers <abhishekabhishek87810@gmail.com>
2023-08-23 17:30:58 +0530

    adicecommit

---
+++ src/components/Mains.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../images/logo.jpg';
+// import './App.css';
+
+function Main() {
+  return (
+    <div className='container'>
+    <div className='abhi1'>
+    <div className='abhi'>
+      <img src={logo} alt="Paris" />
+        <p>welcome in main page</p>
+    </div>
+    <div className='row'>
+      <div className="col-md-7">
+        <div className='type_two'>
+          <p>Lorem Ipsum dolor sit amet, conse ctetur adipiscing elit</p>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Main
